bab280ba9f5b1544622c1328ae17a49b66111ee7
FrostIce482 <frostice48260@gmail.com>
2023-12-20 13:36:05 +0700

    export cli scripts

---
+++ src/cli/index.ts
@@ -0,0 +1,9 @@
+import _cliInit from "./init.js"
+import _cliPack from "./pack.js"
+
+export namespace BedrockPackCLI {
+    export const init = _cliInit
+    export const pack = _cliPack
+}
+
+export default BedrockPackCLI
