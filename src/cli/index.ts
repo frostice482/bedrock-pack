94b98e50fb33ebc28f8860cad8cae7553a2a74e7
FrostIce482 <frostice48260@gmail.com>
2024-01-05 11:48:30 +0700

    remove cli feature
    will be implemented in different repo

--- src/cli/index.ts
+++
@@ -1,6 +0,0 @@
-export namespace BedrockPackCLI {
-    export const init: typeof import('./init.js').default = (...args) => { return require('./init.js').default.apply(undefined, args) }
-    export const pack: typeof import('./pack.js').default = (...args) => { return require('./pack.js').default.apply(undefined, args) }
-}
-
-export default BedrockPackCLI
